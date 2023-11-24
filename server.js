709910b28e0f259f7f58ba0cb12972383735591f
Sarah Leder <Trinity16201@gmail.com>
2023-11-24 15:58:38 -0500

    updated the employee role function

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ function addARole() {
       {
         type: "input",
         name: "department",
-        message: "What is the department id?",
+        message: "What is the department id? (Please choose option 1-4)",
       },
     ])
     .then((answer) => {
@@ -156,41 +156,85 @@ function addARole() {
 }
 
 function addEmployee() {
-  const addEmployeeQuery = inquirer
-    .prompt({
-      type: "input",
-      name: "firstname",
-      message: "Please enter the first name of the new employee.",
-    })
-    .prompt({
-      type: "input",
-      name: "lastname",
-      message: "Please enter the last name of the new employee.",
-    })
-    .prompt({
-      type: "input",
-      name: "role_id",
-      message: "Please enter the role id as a number.",
-    })
-    .prompt({
-      type: "input",
-      name: "manager id",
-      message: "Please enter the manager id as a number",
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstname",
+        message: "Please enter the first name of the new employee.",
+      },
+      {
+        type: "input",
+        name: "lastname",
+        message: "Please enter the last name of the new employee.",
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "Please enter the role id as a number. Choose between 1-4.",
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "Please enter the manager id as a number. Choose between 1-4.",
+      },
+    ])
+    .then((answer) => {
+      const insertEmployee = "INSERT INTO employees SET ?";
+      connection.query(
+        insertEmployee,
+        {
+          first_name: answer.firstname,
+          last_name: answer.lastname,
+          role_id: answer.role,
+          manager_id: answer.manager,
+        },
+        (err, res) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Added " + answer.firstname + " to the database.");
+          start();
+        }
+      );
     });
-
-  connection.query(query, (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    addEmployeeQuery();
-  });
 }
 function updateEmployeeRole() {
-  const updateEmployeeRoleQuery = "SELECT ";
-  connection.query(query, (err, res) => {
-    if (err) throw err;
-    console.table(res);
-    updateEmployeeRoleQuery();
-  });
+    const employeeList = "SELECT * FROM employees";
+    connection.query(employeeList, (err, res) => {
+        employeeList = response.map(employees => ({
+            name: employees.first_name.concat(" ", employees.last_name),
+            value: employees.id
+        }));
+    })
+    return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeChoice",
+        message: "Which employee would you like to change their role to?",
+        choices: employeeList
+      },
+      {
+        type: "input",
+        name: "roleUpdate",
+        message: "What role would you like to update this employee to? (Choose 1-4)",
+      },
+    ]).then((answer) => {
+        const updateEmployee = `UPDATE employee SET role_id= ${answer.roleUpdate}`;
+        connection.query(
+            updateEmployee,
+        (err, res) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("Updated employee in the database.");
+          start();
+        }
+        );
+    });
 }
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
